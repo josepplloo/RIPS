fa37a3dbe91f901ed349f4380eadac5c3aa1c7d1
josepplloo <josepplloo@gmail.com>
2017-07-14 10:23:20 -0500

    add a underbrush split by dx

--- underbrush.py
+++ underbrush.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 infile ="/Users/josepplloo/Documents/scripts/xaa_clean.csv"
 df = pd.read_csv(infile, header = 0)
-df = df[df['DxPrincipal']==1333]
+for aux in df.DxPrincipal.unique():
+    print aux
+    bee = df[df['DxPrincipal']==aux]
+    bee.to_csv("/Users/josepplloo/Documents/scripts/RIPS_2013_1/splitDx/%s.csv" %aux,index=False)
 
 
-#df.to_csv("/Users/josepplloo/Documents/RIP2013/scripts/xaa_clean1333.csv",index=False)
 
 
-
-
-#debo de filtar la data para solo un procedimiento y mirar el arbol para ese procedimiento solo
+#debo de filtar la data para solo un diagnostico
